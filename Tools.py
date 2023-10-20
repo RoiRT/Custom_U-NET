5202fc1fa1e5fb9088dd452bfac392459287ba89
RoiRT <roirt2809@gmail.com>
2023-10-20 18:04:20 +0200

    WIP

---
+++ Tools.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+import torchvision
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import json
+
+abs_path = 'C:\\Proxectos\\Custom_U-NET\\g.v2i.coco-segmentation'
+
+def read_dataset(path, n=-1):
+    features, labels = [], []
+    transform = transforms.ToTensor()
+    with open(path + '/_annotations.coco.json', 'r') as file:
+        data = json.load(file)
+    for i, image in enumerate(data['images']):
+        if i == n: break
+        features.append(transform(Image.open(os.path.join(path, str(image['file_name'])))))
+        annotations = list(filter(lambda annotation: annotation.get('image_id') == image['id'], data['annotations']))
+        label = np.zeros((image['height'], image['width']), dtype=np.uint8)
+        for annotation in annotations:
+            pts = np.array(annotation.get('segmentation'), dtype=np.int32)
+            pts = pts.reshape((1, int(pts.shape[-1] / 2), 2))
+            cv2.fillPoly(label, pts, 1)
+
+        labels.append(torch.from_numpy(label.astype(np.int64)))
+
+    return features, labels
+
+def rand_crop(feature, label, height, width):
+    """Randomly crop both feature and label images."""
+    rect = torchvision.transforms.RandomCrop.get_params(
+        feature, (height, width))
+    feature = torchvision.transforms.functional.crop(feature, *rect)
+    label = torchvision.transforms.functional.crop(label, *rect)
+    return feature, label
+
+
+
+class GunsDataset(torch.utils.data.Dataset):
+    def __init__(self, crop_size, dir):
+        self.transform = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        self.crop_size = crop_size
+        features, self.labels = read_dataset(os.path.join(abs_path, dir))
+        self.features = [self.normalize_image(feature) for feature in self.filter(features)]
+
+        print('read ' + str(len(self.labels)) + ' examples')
+
+    def normalize_image(self, img):
+        return self.transform(img.float() / 255.0)
+
+    def filter(self, imgs):
+        return [img for img in imgs if (
+                img.shape[1] >= self.crop_size[0] and
+                img.shape[2] >= self.crop_size[1])]
+
+    def __getitem__(self, idx):
+        feature, label = rand_crop(self.features[idx], self.labels[idx], *self.crop_size)
+        return feature, label
+
+    def __len__(self):
+        return len(self.labels)
--- U_net.py
+++ U_net.py
@@ -7,6 +7,7 @@ import numpy as np
 
 pretrained_net_cod = torchvision.models.resnet18(weights='ResNet18_Weights.IMAGENET1K_V1')
 resnet_cod_layers = list(pretrained_net_cod.children())[:-2]
+path = 'C:\\Proxectos\\Custom_U-NET\\g.v2i.coco-segmentation'
 
 def flatten_layers(model):
     flat_layers = []
@@ -102,7 +103,7 @@ class ResNet(nn.Module):
                 self.net[-1].feedforward.weight.data.copy_(torch.flip(resnet_weight[-(int(i/2)+1)][1 - i % 2][-1][0].weight,(2, 3)));
 
         self.net.add_module('top',
-                            nn.ConvTranspose2d(num_channels[-1], num_classes, 7, stride=2, padding=3, output_padding=1, bias=False))
+                            nn.ConvTranspose2d(num_channels[-1], num_classes, 7, stride=4, padding=3, output_padding=3, bias=False))
 
         resize_weights = np.resize(resnet_weight[0].weight.detach(), (64, num_classes, 7, 7))
         # self.net[-1].weight.data = nn.Parameter(torch.flip(torch.tensor(resize_weights), (2, 3)))
@@ -111,13 +112,17 @@ class ResNet(nn.Module):
         return self.net(X)
 
 def create_deco():
+    input_shape = (512, 32, 32)
     resnet_dec = ResNet((512, 256, 256, 128, 128, 64, 64, 64), 2)
-    summary(resnet_dec, (512, 32, 32), device='cpu')
+    summary(resnet_dec, input_shape, device='cpu')
+    print('Input shape = '+str(input_shape))
     return resnet_dec
 
 def create_unet():
+    input_shape = (3, 512, 512)
     net = nn.Sequential(*resnet_cod_layers)
     resnet_dec = ResNet((512, 256, 256, 128, 128, 64, 64, 64), 2)
     net.add_module('deco', resnet_dec)
-    summary(net, (3, 512, 512), device='cpu')
+    print(summary(net, input_shape, device='cpu'))
+    print('Input shape = '+str(input_shape))
     return net
